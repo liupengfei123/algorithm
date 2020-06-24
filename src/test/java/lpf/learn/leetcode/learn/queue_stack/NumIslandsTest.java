582b8df9f0a7c97d21ebc786165105a19e7a0198
liupfengfei <liupf@bonree.com>
2020-06-24 11:35:10 +0800

    1、leetcode 队列栈练习

---
+++ src/test/java/lpf/learn/leetcode/learn/queue_stack/NumIslandsTest.java
@@ -0,0 +1,18 @@
+package lpf.learn.leetcode.learn.queue_stack;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NumIslandsTest {
+
+    @Test
+    public void test() {
+        NumIslands numIslands = new NumIslands(); // 设置长度为 3
+
+        Assert.assertEquals(1, numIslands.numIslands(new char[][]{"11110".toCharArray(), "11010".toCharArray(), "11000".toCharArray(), "00000".toCharArray()}));
+
+        Assert.assertEquals(3, numIslands.numIslands(new char[][]{"11000".toCharArray(), "11000".toCharArray(), "00100".toCharArray(), "00011".toCharArray()}));
+
+        Assert.assertEquals(1, numIslands.numIslands(new char[][]{"1".toCharArray()}));
+    }
+}
