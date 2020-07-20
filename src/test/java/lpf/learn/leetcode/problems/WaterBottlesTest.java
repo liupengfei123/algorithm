798fcb703f8c35791024e5e9169a10641289bbbc
liupfengfei <liupf@bonree.com>
2020-07-20 17:54:50 +0800

    1、leetcode 栈练习

---
+++ src/test/java/lpf/learn/leetcode/problems/WaterBottlesTest.java
@@ -0,0 +1,20 @@
+package lpf.learn.leetcode.problems;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class WaterBottlesTest {
+    @Test
+    public void test() {
+
+        WaterBottles temp = new WaterBottles();
+
+
+        Assert.assertEquals(17, temp.numWaterBottles(15, 8));
+
+
+    }
+
+
+}
