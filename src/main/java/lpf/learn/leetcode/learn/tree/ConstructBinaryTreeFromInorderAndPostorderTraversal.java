5b963889fe01051aef0d816f137d1509412ba4b0
liupfengfei <liupf@bonree.com>
2020-08-05 18:03:06 +0800

    1、leetcode 树练习 [108]将有序数组转换为二叉搜索树 [106]从中序与后序遍历序列构造二叉树

--- src/main/java/lpf/learn/leetcode/learn/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
+++ src/main/java/lpf/learn/leetcode/learn/tree/ConstructBinaryTreeFromInorderAndPostorderTraversal.java
@@ -4,24 +4,21 @@ import lpf.learn.leetcode.entity.TreeNode;
 
 import java.util.HashMap;
 
-/**
- * 105. 从前序与中序遍历序列构造二叉树
- *
- * 根据一棵树的前序遍历与中序遍历构造二叉树。
- *
- * 注意:你可以假设树中没有重复的元素。
- *
+/** [106]从中序与后序遍历序列构造二叉树
+ * 根据一棵树的中序遍历与后序遍历构造二叉树。
+ * 注意:
+ * 你可以假设树中没有重复的元素。
  * 例如，给出
- * 前序遍历 preorder = [3,9,20,15,7]
- * 中序遍历 inorder = [9,3,15,20,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 后序遍历 postorder = [9,15,7,20,3]
  * 返回如下的二叉树：
  *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ *   / \
+ *  9  20
+ *    /  \
+ *   15   7
  */
-public class BuildTreeByInorderAndPostorder {
+public class ConstructBinaryTreeFromInorderAndPostorderTraversal {
     private int[] postorder;
     private int postIndex;
     private HashMap<Integer, Integer> idx_map = new HashMap<>();
