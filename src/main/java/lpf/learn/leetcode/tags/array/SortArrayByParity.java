2b1a37147c1e67a24b690b2911d1bf632a9508b9
liupfengfei <liupf@bonree.com>
2020-09-21 17:35:12 +0800

    1、leetcode 数组练习 [840]矩阵中的幻方 [905]按奇偶排序数组

---
+++ src/main/java/lpf/learn/leetcode/tags/array/SortArrayByParity.java
@@ -0,0 +1,34 @@
+package lpf.learn.leetcode.tags.array;
+
+/** [905]按奇偶排序数组
+ * 给定一个非负整数数组 A，返回一个数组，在该数组中， A 的所有偶数元素之后跟着所有奇数元素。
+ * 你可以返回满足此条件的任何数组作为答案。
+ *
+ * 示例：
+ * 输入：[3,1,2,4]
+ * 输出：[2,4,3,1]
+ * 输出 [4,2,3,1]，[2,4,1,3] 和 [4,2,1,3] 也会被接受。
+ *
+ * 提示：
+ * 1 <= A.length <= 5000
+ * 0 <= A[i] <= 5000
+ */
+public class SortArrayByParity {
+    public int[] sortArrayByParity(int[] A) {
+        int left = 0, right = A.length -1;
+
+        while (left < right) {
+            int temp = A[left];
+            if (temp % 2 == 0) {
+                left++;
+                continue;
+            }
+            while (left < right && A[right] % 2 == 1) {
+                right--;
+            }
+            A[left++] = A[right];
+            A[right--] = temp;
+        }
+        return A;
+    }
+}
