53825b03c501a606c22fa0bd0540ba8db6207ed0
liupfengfei <liupf@bonree.com>
2020-11-04 14:09:33 +0800

    1、leetcode 链表练习 [876]链表的中间结点

--- src/main/java/lpf/learn/leetcode/tags/linked/MiddleOfTheLinkedList.java
+++ src/main/java/lpf/learn/leetcode/tags/linked/MiddleOfTheLinkedList.java
@@ -2,10 +2,11 @@ package lpf.learn.leetcode.tags.linked;
 
 import lpf.learn.leetcode.entity.ListNode;
 
-/** [876]链表的中间结点
+/**
+ * [876]链表的中间结点
  * 给定一个头结点为 head 的非空单链表，返回链表的中间结点。
  * 如果有两个中间结点，则返回第二个中间结点。
- *
+ * <p>
  * 示例 1：
  * 输入：[1,2,3,4,5]
  * 输出：此列表中的结点 3 (序列化形式：[3,4,5])
@@ -13,12 +14,12 @@ import lpf.learn.leetcode.entity.ListNode;
  * 注意，我们返回了一个 ListNode 类型的对象 ans，这样：
  * ans.val = 3, ans.next.val = 4, ans.next.next.val = 5, 以及 ans.next.next.next =
  * NULL.
- *
+ * <p>
  * 示例 2：
  * 输入：[1,2,3,4,5,6]
  * 输出：此列表中的结点 4 (序列化形式：[4,5,6])
  * 由于该列表有两个中间结点，值分别为 3 和 4，我们返回第二个结点。
- *
+ * <p>
  * 提示：
  * 给定链表的结点数介于 1 和 100 之间。
  */
@@ -33,7 +34,23 @@ public class MiddleOfTheLinkedList {
             if (fast.next != null) {
                 fast = fast.next;
             }
-}
+        }
+        return slow;
+    }
+
+    // 如果有两个中间结点，则返回第一个中间结点。
+    public ListNode middleNode2(ListNode head) {
+        ListNode fast = head, slow = head;
+
+        while (fast.next != null) {
+            fast = fast.next;
+            if (fast.next != null) {
+                fast = fast.next;
+            } else {
+                break;
+            }
+            slow = slow.next;
+        }
         return slow;
     }
 }
