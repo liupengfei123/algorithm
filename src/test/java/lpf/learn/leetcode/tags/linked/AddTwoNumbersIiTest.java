56c3a368345a864afbd2aa443c01149599596e59
liupfengfei <liupf@bonree.com>
2020-11-09 15:13:14 +0800

    1、leetcode 链表练习 [445]两数相加 II

--- src/test/java/lpf/learn/leetcode/tags/linked/AddTwoNumbersIiTest.java
+++ src/test/java/lpf/learn/leetcode/tags/linked/AddTwoNumbersIiTest.java
@@ -14,24 +14,15 @@ public class AddTwoNumbersIiTest {
 
     @Test
     public void test2(){
-        ReverseLinkedListIi test = new ReverseLinkedListIi();
+        AddTwoNumbersIi test = new AddTwoNumbersIi();
         SerializeAndDeserializeBinaryLinked temp = new SerializeAndDeserializeBinaryLinked();
-        Assert.assertEquals(temp.deserialize("6,5,4,3,2,1"), test.reverseBetween(temp.deserialize("1,2,3,4,5,6"), 1,6));
+        Assert.assertEquals(temp.deserialize("0"), test.addTwoNumbers(temp.deserialize("0"), temp.deserialize("0")));
     }
 
     @Test
     public void test3(){
-        ReverseLinkedListIi test = new ReverseLinkedListIi();
+        AddTwoNumbersIi test = new AddTwoNumbersIi();
         SerializeAndDeserializeBinaryLinked temp = new SerializeAndDeserializeBinaryLinked();
-        Assert.assertEquals(temp.deserialize("1,2,3,4,5,6"), test.reverseBetween(temp.deserialize("1,2,3,4,5,6"), 1,1));
+        Assert.assertEquals(temp.deserialize("1,0,0,9"), test.addTwoNumbers(temp.deserialize("1,0"), temp.deserialize("9,9,9")));
     }
-
-    @Test
-    public void test4(){
-        PartitionList test = new PartitionList();
-        SerializeAndDeserializeBinaryLinked temp = new SerializeAndDeserializeBinaryLinked();
-        Assert.assertEquals(temp.deserialize("1,4,4,3,2,5,2"), test.partition(temp.deserialize("4,1,4,3,2,5,2"), 2));
-    }
-
-
 }
