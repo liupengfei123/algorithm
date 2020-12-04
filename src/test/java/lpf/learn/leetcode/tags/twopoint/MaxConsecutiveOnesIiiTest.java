bdd7fce93f1c73626a5a0322af4e5acc1dd893f6
liupfengfei <liupf@bonree.com>
2020-12-04 17:58:31 +0800

    1、leetcode 双指针练习 [1004]最大连续1的个数 III

---
+++ src/test/java/lpf/learn/leetcode/tags/twopoint/MaxConsecutiveOnesIiiTest.java
@@ -0,0 +1,50 @@
+package lpf.learn.leetcode.tags.twopoint;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MaxConsecutiveOnesIiiTest {
+    @Test
+    public void test1(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(6, test.longestOnes(new int[]{1,1,1,0,0,0,1,1,1,1,0}, 2));
+    }
+
+    @Test
+    public void test2(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(10, test.longestOnes(new int[]{0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1}, 3));
+    }
+
+    @Test
+    public void test3(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(3, test.longestOnes(new int[]{0,0,0,0,0,0,0,0}, 3));
+    }
+
+    @Test
+    public void test4(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(4, test.longestOnes(new int[]{0,0,0,0,0,0,0,0,1}, 3));
+    }
+
+    @Test
+    public void test5(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(9, test.longestOnes(new int[]{0,0,0,0,1,1,1,1,1}, 6));
+    }
+
+
+    @Test
+    public void test6(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(32, test.longestOnes(new int[]{1,0,0,1,0,0,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,1,1,0,1,1,1,0,0,1,1,1,0,0,1,0,1,0,0,1,0,0,1,1}, 9));
+    }
+
+
+    @Test
+    public void test7(){
+        MaxConsecutiveOnesIii test = new MaxConsecutiveOnesIii();
+        Assert.assertEquals(15, test.longestOnes(new int[]{1,0,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1}, 3));
+    }
+}
