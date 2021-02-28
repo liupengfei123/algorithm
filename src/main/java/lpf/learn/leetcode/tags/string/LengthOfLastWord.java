7a7ee1009b82c5c5786d3255f146571362493647
liupf <506629790@qq.com>
2021-02-28 17:53:33 +0800

    1、leetcode 字符串练习 [58]最后一个单词的长度

---
+++ src/main/java/lpf/learn/leetcode/tags/string/LengthOfLastWord.java
@@ -0,0 +1,28 @@
+package lpf.learn.leetcode.tags.string;
+
+/** [58]最后一个单词的长度
+ *  给你一个字符串 s，由若干单词组成，单词之间用空格隔开。返回字符串中最后一个单词的长度。如果不存在最后一个单词，请返回 0 。
+ *  单词 是指仅由字母组成、不包含任何空格字符的最大子字符串。
+ *
+ * 示例 1：
+ * 输入：s = "Hello World"
+ * 输出：5
+ *
+ * 示例 2：
+ * 输入：s = " "
+ * 输出：0
+ *
+ *  提示：
+ *  1 <= s.length <= 104
+ *  s 仅有英文字母和空格 ' ' 组成
+ *
+ * @author liupf
+ * @date 2021-02-28 17:49
+ */
+public class LengthOfLastWord {
+
+    public int lengthOfLastWord(String s) {
+        s = s.trim();
+        return s.length() - s.lastIndexOf(' ') - 1;
+    }
+}
