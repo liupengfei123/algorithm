c0f3bb890d2062e5e4e9cafdb9ef36241cc663bc
liupf <506629790@qq.com>
2021-04-11 15:12:21 +0800

    1、leetcode 字符串练习 [383]赎金信

---
+++ src/main/java/lpf/learn/leetcode/tags/string/RansomNote.java
@@ -0,0 +1,43 @@
+package lpf.learn.leetcode.tags.string;
+
+/** [383]赎金信
+ * 给定一个赎金信 (ransom) 字符串和一个杂志(magazine)字符串，判断第一个字符串 ransom 能不能由第二个字符串 magazines 里面的字符构成。如果可以构成，返回 true ；否则返回 false。
+ * (题目说明：为了不暴露赎金信字迹，要从杂志上搜索各个需要的字母，组成单词来表达意思。杂志字符串中的每个字符只能在赎金信字符串中使用一次。)
+ *
+ * 示例 1：
+ * 输入：ransomNote = "a", magazine = "b"
+ * 输出：false
+ *
+ * 示例 2：
+ * 输入：ransomNote = "aa", magazine = "ab"
+ * 输出：false
+ *
+ * 示例 3：
+ * 输入：ransomNote = "aa", magazine = "aab"
+ * 输出：true
+ *
+ * 提示：
+ * 你可以假设两个字符串均只含有小写字母。
+ * @author liupf
+ * @date 2021-04-11 15:08
+ */
+public class RansomNote {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int[] ransomCount = new int[26];
+        int[] magazineCount = new int[26];
+
+        for (char c : ransomNote.toCharArray()) {
+            ransomCount[c - 'a']++;
+        }
+        for (char c : magazine.toCharArray()) {
+            magazineCount[c - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (ransomCount[i] > magazineCount[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
