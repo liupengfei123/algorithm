3aab1514fdf67f08423abb5a14ae8e8b3872ce73
刘鹏飞 <liupf@bonree.com>
2021-04-26 10:12:00 +0800

    1、leetcode 数组练习 1011]在 D 天内送达包裹的能力

---
+++ src/test/java/lpf/learn/leetcode/tags/array/CapacityToShipPackagesWithinDDaysTest.java
@@ -0,0 +1,30 @@
+package lpf.learn.leetcode.tags.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author liupf
+ * @date 2020-09-07 21:45
+ */
+public class CapacityToShipPackagesWithinDDaysTest {
+
+    @Test
+    public void test1(){
+        CapacityToShipPackagesWithinDDays test = new CapacityToShipPackagesWithinDDays();
+        Assert.assertEquals(15, test.shipWithinDays(new int[]{1,2,3,4,5,6,7,8,9,10}, 5));
+    }
+
+    @Test
+    public void test2(){
+        CapacityToShipPackagesWithinDDays test = new CapacityToShipPackagesWithinDDays();
+        Assert.assertEquals(6, test.shipWithinDays(new int[]{3,2,2,4,1,4}, 3));
+    }
+
+    @Test
+    public void test3(){
+        CapacityToShipPackagesWithinDDays test = new CapacityToShipPackagesWithinDDays();
+        Assert.assertEquals(3, test.shipWithinDays(new int[]{1,2,3,1,1}, 4));
+    }
+
+}
