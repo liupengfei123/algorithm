b5a325a243eac5fe15f9e2a01c0c26cee8909ebe
liupf <506629790@qq.com>
2021-05-09 18:25:29 +0800

    1、leetcode 二分查找法练习 [1482]制作 m 束花所需的最少天数

---
+++ src/test/java/lpf/learn/leetcode/tags/BinarySearch/MinimumNumberOfDaysToMakeMBouquetsTest.java
@@ -0,0 +1,46 @@
+package lpf.learn.leetcode.tags.BinarySearch;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author liupf
+ * @date 2021-05-09 18:17
+ */
+public class MinimumNumberOfDaysToMakeMBouquetsTest {
+    @Test
+    public void test1(){
+        MinimumNumberOfDaysToMakeMBouquets test = new MinimumNumberOfDaysToMakeMBouquets();
+        Assert.assertEquals(3, test.minDays(new int[]{1,10,3,10,2}, 3, 1));
+    }
+
+    @Test
+    public void test2(){
+        MinimumNumberOfDaysToMakeMBouquets test = new MinimumNumberOfDaysToMakeMBouquets();
+        Assert.assertEquals(-1, test.minDays(new int[]{1,10,3,10,2}, 3, 2));
+    }
+
+    @Test
+    public void test3(){
+        MinimumNumberOfDaysToMakeMBouquets test = new MinimumNumberOfDaysToMakeMBouquets();
+        Assert.assertEquals(10, test.minDays(new int[]{1,10,3,10,2}, 2, 2));
+    }
+
+    @Test
+    public void test4(){
+        MinimumNumberOfDaysToMakeMBouquets test = new MinimumNumberOfDaysToMakeMBouquets();
+        Assert.assertEquals(2, test.minDays(new int[]{1,10,3,10,2}, 2, 1));
+    }
+
+    @Test
+    public void test5(){
+        MinimumNumberOfDaysToMakeMBouquets test = new MinimumNumberOfDaysToMakeMBouquets();
+        Assert.assertEquals(12, test.minDays(new int[]{7,7,7,7,12,7,7}, 2, 3));
+    }
+
+    @Test
+    public void test6(){
+        MinimumNumberOfDaysToMakeMBouquets test = new MinimumNumberOfDaysToMakeMBouquets();
+        Assert.assertEquals(1000000000, test.minDays(new int[]{1000000000,1000000000}, 1, 1));
+    }
+}
