cc9d64f6edc773249c39638addb9776a20edd1f0
刘鹏飞 <liupf@bonree.com>
2021-05-17 11:35:12 +0800

    1、leetcode 动态规划练习 [1105]填充书架

---
+++ src/test/java/lpf/learn/leetcode/tags/dp/FillingBookcaseShelvesTest.java
@@ -0,0 +1,45 @@
+package lpf.learn.leetcode.tags.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FillingBookcaseShelvesTest {
+    @Test
+    public void test1(){
+        FillingBookcaseShelves test = new FillingBookcaseShelves();
+        Assert.assertEquals(6, test.minHeightShelves(new int[][]{{1,1},{2,3},{2,3},{1,1},{1,1},{1,1},{1,2}}, 4));
+    }
+
+    @Test
+    public void test2(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(6, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void test3(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(10, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6}));
+    }
+
+    @Test
+    public void test4(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(11, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6, 8, 10}));
+    }
+
+    @Test
+    public void test5(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(3, test.numberOfArithmeticSlices(new int[]{7, 7, 7, 7}));
+    }
+
+    @Test
+    public void test6(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(0, test.numberOfArithmeticSlices(new int[]{1, 1, 2, 5, 7}));
+    }
+
+
+
+}
