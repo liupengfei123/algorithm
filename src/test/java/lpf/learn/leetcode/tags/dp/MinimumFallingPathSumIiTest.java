71e96394ebb672fb88fd44e3b5f7031cd01ef53b
刘鹏飞 <liupf@bonree.com>
2021-05-20 10:23:46 +0800

    1、leetcode 动态规划练习 [1289]下降路径最小和  II

---
+++ src/test/java/lpf/learn/leetcode/tags/dp/MinimumFallingPathSumIiTest.java
@@ -0,0 +1,45 @@
+package lpf.learn.leetcode.tags.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinimumFallingPathSumIiTest {
+    @Test
+    public void test1(){
+        MinimumFallingPathSumIi test = new MinimumFallingPathSumIi();
+        Assert.assertEquals(13, test.minFallingPathSum(new int[][]{{1,2,3},{4,5,6},{7,8,9}}));
+    }
+
+    @Test
+    public void test2(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(6, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5}));
+    }
+
+    @Test
+    public void test3(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(10, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6}));
+    }
+
+    @Test
+    public void test4(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(11, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6, 8, 10}));
+    }
+
+    @Test
+    public void test5(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(3, test.numberOfArithmeticSlices(new int[]{7, 7, 7, 7}));
+    }
+
+    @Test
+    public void test6(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(0, test.numberOfArithmeticSlices(new int[]{1, 1, 2, 5, 7}));
+    }
+
+
+
+}
