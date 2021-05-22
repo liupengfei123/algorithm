b483c1fac6c9a18a80c3240b3f7f9b7fcd4ac724
liupf <506629790@qq.com>
2021-05-22 18:28:09 +0800

    1、leetcode 动态规划练习 [1140]石子游戏 II

---
+++ src/test/java/lpf/learn/leetcode/tags/dp/StoneGameIiTest.java
@@ -0,0 +1,45 @@
+package lpf.learn.leetcode.tags.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StoneGameIiTest {
+    @Test
+    public void test1(){
+        StoneGameIi test = new StoneGameIi();
+        Assert.assertEquals(3, test.stoneGameII(new int[]{1, 2, 3, 4}));
+    }
+
+    @Test
+    public void test2(){
+        StoneGameIi test = new StoneGameIi();
+        Assert.assertEquals(10, test.stoneGameII(new int[]{2,7,9,4,4}));
+    }
+
+    @Test
+    public void test3(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(10, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6}));
+    }
+
+    @Test
+    public void test4(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(11, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6, 8, 10}));
+    }
+
+    @Test
+    public void test5(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(3, test.numberOfArithmeticSlices(new int[]{7, 7, 7, 7}));
+    }
+
+    @Test
+    public void test6(){
+        ArithmeticSlices test = new ArithmeticSlices();
+        Assert.assertEquals(0, test.numberOfArithmeticSlices(new int[]{1, 1, 2, 5, 7}));
+    }
+
+
+
+}
