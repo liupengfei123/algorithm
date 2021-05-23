12f350c74944e023077da7fb2c26025bc83324ed
liupf <506629790@qq.com>
2021-05-23 13:19:16 +0800

    1、leetcode 周赛 5764. 准时到达的列车最小时速

---
+++ src/test/java/lpf/learn/leetcode/game/MinimumSpeedtoArriveonTimeTest.java
@@ -0,0 +1,37 @@
+package lpf.learn.leetcode.game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MinimumSpeedtoArriveonTimeTest {
+    @Test
+    public void test1(){
+        MinimumSpeedtoArriveonTime test = new MinimumSpeedtoArriveonTime();
+        Assert.assertEquals(1, test.minSpeedOnTime(new int[]{1,3,2}, 6));
+    }
+
+    @Test
+    public void test2(){
+        MinimumSpeedtoArriveonTime test = new MinimumSpeedtoArriveonTime();
+        Assert.assertEquals(3, test.minSpeedOnTime(new int[]{1,3,2}, 2.7));
+    }
+
+    @Test
+    public void test3(){
+        MinimumSpeedtoArriveonTime test = new MinimumSpeedtoArriveonTime();
+        Assert.assertEquals(10, test.minSpeedOnTime(new int[]{1,3,2}, 2.2));
+    }
+
+    @Test
+    public void test4(){
+        MinimumSpeedtoArriveonTime test = new MinimumSpeedtoArriveonTime();
+        Assert.assertEquals(-1, test.minSpeedOnTime(new int[]{1,3,2}, 1.9));
+    }
+
+    @Test
+    public void test5(){
+        MinimumSpeedtoArriveonTime test = new MinimumSpeedtoArriveonTime();
+        Assert.assertEquals(10000000, test.minSpeedOnTime(new int[]{1,1,100000}, 2.01));
+    }
+
+}
