34f09ebb60c915ef1cd99e8f71e75b048a647a12
刘鹏飞 <liupf@bonree.com>
2021-05-25 10:52:17 +0800

    1、leetcode 周赛 [1872]石子游戏 VIII

---
+++ src/test/java/lpf/learn/leetcode/game/StoneGameViiiTest.java
@@ -0,0 +1,24 @@
+package lpf.learn.leetcode.game;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StoneGameViiiTest {
+    @Test
+    public void test1(){
+        StoneGameViii test = new StoneGameViii();
+        Assert.assertEquals(5, test.stoneGameVIII(new int[]{-1,2,-3,4,-5}));
+    }
+
+    @Test
+    public void test2(){
+        StoneGameViii test = new StoneGameViii();
+        Assert.assertEquals(13, test.stoneGameVIII(new int[]{7,-6,5,10,5,-2,-6}));
+    }
+
+    @Test
+    public void test3(){
+        StoneGameViii test = new StoneGameViii();
+        Assert.assertEquals(-22, test.stoneGameVIII(new int[]{-10,-12}));
+    }
+}
