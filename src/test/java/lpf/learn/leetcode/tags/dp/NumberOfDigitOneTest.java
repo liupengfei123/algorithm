a822efe0e455d17a36481a5bcc76dcfac2581d6f
刘鹏飞 <liupf@bonree.com>
2021-08-13 15:02:32 +0800

    1、leetcode 数学练习 [233]数字 1 的个数

---
+++ src/test/java/lpf/learn/leetcode/tags/dp/NumberOfDigitOneTest.java
@@ -0,0 +1,50 @@
+package lpf.learn.leetcode.tags.dp;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class NumberOfDigitOneTest {
+    @Test
+    public void test1(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(1, test.countDigitOne(1));
+    }
+
+    @Test
+    public void test2(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(0, test.countDigitOne(0));
+    }
+
+
+    @Test
+    public void test3(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(1, test.countDigitOne(8));
+    }
+
+
+    @Test
+    public void test4(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(6, test.countDigitOne(13));
+    }
+
+    @Test
+    public void test5(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(761, test.countDigitOne(1300));
+    }
+
+    @Test
+    public void test6(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(900000001, test.countDigitOne(1000000000));
+    }
+
+    @Test
+    public void test7(){
+        NumberOfDigitOne test = new NumberOfDigitOne();
+        Assert.assertEquals(604225078, test.countDigitOne(620236879));
+    }
+}
