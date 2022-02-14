0211d69bd14b0f923eda3c33871ce66f31cd335b
刘鹏飞 <liupf@bonree.com>
2022-02-14 11:41:42 +0800

    1、leetcode 数组练习 [540]有序数组中的单一元素

---
+++ src/test/java/lpf/learn/leetcode/tags/array/SingleElementInASortedArrayTest.java
@@ -0,0 +1,43 @@
+package lpf.learn.leetcode.tags.array;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SingleElementInASortedArrayTest {
+    @Test
+    public void test1(){
+        SingleElementInASortedArray test = new SingleElementInASortedArray();
+        Assert.assertEquals(2, test.singleNonDuplicate(new int[]{1,1,2,3,3,4,4,8,8}));
+    }
+
+    @Test
+    public void test2(){
+        SingleElementInASortedArray test = new SingleElementInASortedArray();
+        Assert.assertEquals(5, test.singleNonDuplicate(new int[]{1,1,3,3,4,4,5,8,8}));
+    }
+
+    @Test
+    public void test3(){
+        SingleElementInASortedArray test = new SingleElementInASortedArray();
+        Assert.assertEquals(10, test.singleNonDuplicate(new int[]{3,3,7,7,10,11,11}));
+    }
+
+    @Test
+    public void test4(){
+        SingleElementInASortedArray test = new SingleElementInASortedArray();
+        Assert.assertEquals(4, test.singleNonDuplicate(new int[]{3,3,4,7,7,11,11}));
+    }
+
+
+    @Test
+    public void test5(){
+        SingleElementInASortedArray test = new SingleElementInASortedArray();
+        Assert.assertEquals(1, test.singleNonDuplicate(new int[]{1}));
+    }
+
+    @Test
+    public void test6(){
+        SingleElementInASortedArray test = new SingleElementInASortedArray();
+        Assert.assertEquals(2, test.singleNonDuplicate(new int[]{1,1,2}));
+    }
+}
