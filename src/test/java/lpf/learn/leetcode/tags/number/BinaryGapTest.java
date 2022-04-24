215083c9f0f3d635c5bb830eb6a22a95b56da66a
刘鹏飞 <liupf@bonree.com>
2022-04-24 18:08:56 +0800

    1、leetcode 二进制练习 [868]二进制间距

---
+++ src/test/java/lpf/learn/leetcode/tags/number/BinaryGapTest.java
@@ -0,0 +1,37 @@
+package lpf.learn.leetcode.tags.number;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BinaryGapTest {
+    @Test
+    public void test1(){
+        BinaryGap test = new BinaryGap();
+        Assert.assertEquals(2, test.binaryGap(22));
+    }
+
+    @Test
+    public void test2(){
+        BinaryGap test = new BinaryGap();
+        Assert.assertEquals(0, test.binaryGap(8));
+    }
+
+    @Test
+    public void test3(){
+        BinaryGap test = new BinaryGap();
+        Assert.assertEquals(2, test.binaryGap(5));
+    }
+
+
+    @Test
+    public void test4(){
+        BinaryGap test = new BinaryGap();
+        Assert.assertEquals(3, test.binaryGap(513168167));
+    }
+
+    @Test
+    public void test5(){
+        BinaryGap test = new BinaryGap();
+        Assert.assertEquals(5, test.binaryGap(1000000));
+    }
+}
