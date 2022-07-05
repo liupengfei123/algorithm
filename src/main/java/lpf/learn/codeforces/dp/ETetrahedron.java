cf699690f80d07381ed971db82346c3022c36d1f
刘鹏飞 <liupf@bonree.com>
2022-07-05 15:50:36 +0800

    1、codeforces 动态规划练习 166E

--- src/main/java/lpf/learn/codeforces/dp/ETetrahedron.java
+++ src/main/java/lpf/learn/codeforces/dp/ETetrahedron.java
@@ -1,45 +1,30 @@
 package lpf.learn.codeforces.dp;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.util.Scanner;
 
 /**
  *  https://codeforces.com/problemset/problem/166/E
  */
 public class ETetrahedron {
-    public static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
-    public static BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-
+    public static Scanner sc = new Scanner(System.in);
     public static int mod = 1000000007;
 
-    public static void main(String[] args) throws IOException {
-        int n = Integer.parseInt(in.readLine());
-
-        out.print((run(n) + mod) % mod);
-        out.close();
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        System.out.println((run(n) + mod) % mod);
     }
 
     private static int run(int n) {
-        int dV = 1;
-        int oV = 0;
+        long dV = 1;
+        long oV = 0;
 
         for (int i = 1; i <= n; i++) {
-            int nDV = 0;
-            nDV = (nDV + oV) % mod;
-            nDV = (nDV + oV) % mod;
-            nDV = (nDV + oV) % mod;
-
-            int nOV = 0;
-            nOV = (nOV + dV) % mod;
-            nOV = (nOV + oV) % mod;
-            nOV = (nOV + oV) % mod;
+            long nDV = (oV * 3) % mod;
+            long nOV = (dV + oV * 2) % mod;
 
             dV = nDV;
             oV = nOV;
         }
-        return dV;
+        return (int) dV;
     }
 }
