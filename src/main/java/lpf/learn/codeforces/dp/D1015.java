7d72f6f69a18c3656e3a4b2d7d8873a9d96ae2f7
刘鹏飞 <liupf@bonree.com>
2022-07-12 15:51:35 +0800

    1、codeforces D1015

---
+++ src/main/java/lpf/learn/codeforces/dp/D1015.java
@@ -0,0 +1,57 @@
+package lpf.learn.codeforces.dp;
+
+import java.util.Scanner;
+
+/**
+ * https://codeforces.com/problemset/problem/1015/D
+ */
+public class D1015 {
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long s = sc.nextLong();
+        if (!canSuccess(n, k, s)) {
+            System.out.println("no");
+            return;
+        }
+
+        System.out.println("yes");
+        run(n, k, s, 1);
+        System.out.println();
+    }
+
+    private static void run(int n, long k, long s, int index) {
+
+        while (canSuccess(n, k - 1, s - n + 1) && k > 0) {
+            index = nextIndex(n ,index, n - 1);
+            System.out.print(index + " ");
+            s = s - n + 1;
+            k--;
+        }
+        if (k < 1) {
+            return;
+        }
+
+        index = nextIndex(n ,index, (int) (s - k + 1));
+        System.out.print(index + " ");
+
+        for (long l = k - 1; l > 0; l--) {
+            index = nextIndex(n ,index, 1);
+            System.out.print(index + " ");
+        }
+    }
+
+    private static int nextIndex(int n, int index, int step) {
+        if (index + step > n) {
+            return index - step;
+        } else {
+            return index + step;
+        }
+    }
+
+    private static boolean canSuccess(int n, long k, long s) {
+        return k<=s && s<=(n - 1) * k;
+    }
+}
