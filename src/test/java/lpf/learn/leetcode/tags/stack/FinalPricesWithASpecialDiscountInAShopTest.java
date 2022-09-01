8c68a6ec0d589bd2e18dcb1507e02225fbc0b83f
刘鹏飞 <liupf@bonree.com>
2022-09-01 13:33:41 +0800

    1、leetcode 数组练习 1475 商品折扣后的最终价格

---
+++ src/test/java/lpf/learn/leetcode/tags/stack/FinalPricesWithASpecialDiscountInAShopTest.java
@@ -0,0 +1,29 @@
+package lpf.learn.leetcode.tags.stack;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FinalPricesWithASpecialDiscountInAShopTest {
+
+    @Test
+    public void test1(){
+        FinalPricesWithASpecialDiscountInAShop test = new FinalPricesWithASpecialDiscountInAShop();
+
+        Assert.assertArrayEquals(new int[]{4,2,4,2,3}, test.finalPrices(new int[]{8,4,6,2,3}));
+    }
+
+    @Test
+    public void test2(){
+        FinalPricesWithASpecialDiscountInAShop test = new FinalPricesWithASpecialDiscountInAShop();
+
+        Assert.assertArrayEquals(new int[]{1,2,3,4,5}, test.finalPrices(new int[]{1,2,3,4,5}));
+    }
+
+    @Test
+    public void test3(){
+        FinalPricesWithASpecialDiscountInAShop test = new FinalPricesWithASpecialDiscountInAShop();
+
+        Assert.assertArrayEquals(new int[]{9,0,1,6}, test.finalPrices(new int[]{10,1,1,6}));
+    }
+}
