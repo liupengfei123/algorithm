ea2206b2bb72b193fc2edd0abd43cfb7bc742d27
刘鹏飞 <liupf@bonree.com>
2022-10-09 10:42:34 +0800

    1、leetcode 栈练习 856 括号的分数

--- src/main/java/lpf/learn/leetcode/tags/stack/ScoreOfParentheses.java
+++ src/main/java/lpf/learn/leetcode/tags/stack/ScoreOfParentheses.java
@@ -2,52 +2,62 @@ package lpf.learn.leetcode.tags.stack;
 
 import java.util.Stack;
 
-/** [856]括号的分数
- * 给定一个平衡括号字符串 S，按下述规则计算该字符串的分数：
- * () 得 1 分。
- * AB 得 A + B 分，其中 A 和 B 是平衡括号字符串。
- * (A) 得 2 * A 分，其中 A 是平衡括号字符串。
- *
- * 示例 1：
- * 输入： "()"
- * 输出： 1
- *
- * 示例 2：
- * 输入： "(())"
- * 输出： 2
- *
- * 示例 3：
- * 输入： "()()"
- * 输出： 2
- *
- * 示例 4：
- * 输入： "(()(()))"
- * 输出： 6
- *
- * 提示：
- * S 是平衡括号字符串，且只含有 ( 和 ) 。
- * 2 <= S.length <= 50
+/** 856 括号的分数
+ <p>给定一个平衡括号字符串&nbsp;<code>S</code>，按下述规则计算该字符串的分数：</p>
+ <ul>
+ <li><code>()</code> 得 1 分。</li>
+ <li><code>AB</code> 得&nbsp;<code>A + B</code>&nbsp;分，其中 A 和 B 是平衡括号字符串。</li>
+ <li><code>(A)</code> 得&nbsp;<code>2 * A</code>&nbsp;分，其中 A 是平衡括号字符串。</li>
+ </ul>
+
+ <p><strong>示例 1：</strong></p>
+ <pre><strong>输入： </strong>&quot;()&quot;
+ <strong>输出： </strong>1
+ </pre>
+
+ <p><strong>示例 2：</strong></p>
+ <pre><strong>输入： </strong>&quot;(())&quot;
+ <strong>输出： </strong>2
+ </pre>
+
+ <p><strong>示例&nbsp;3：</strong></p>
+ <pre><strong>输入： </strong>&quot;()()&quot;
+ <strong>输出： </strong>2
+ </pre>
+
+ <p><strong>示例&nbsp;4：</strong></p>
+ <pre><strong>输入： </strong>&quot;(()(()))&quot;
+ <strong>输出： </strong>6
+ </pre>
+
+ <p><strong>提示：</strong></p>
+ <ol>
+ <li><code>S</code>&nbsp;是平衡括号字符串，且只含有&nbsp;<code>(</code>&nbsp;和&nbsp;<code>)</code>&nbsp;。</li>
+ <li><code>2 &lt;= S.length &lt;= 50</code></li>
+ </ol>
  */
 public class ScoreOfParentheses {
     public int scoreOfParentheses(String S) {
         Stack<Character> stack = new Stack<>();
         Stack<Integer> valueStack = new Stack<>();
+        char vC = 'V';
 
         for (char c : S.toCharArray()) {
             if (c == '(') {
-                stack.push(c);
-            } else {
-                int value = 0;
-                while (!stack.isEmpty() && stack.peek() == 'v') {
-                    stack.pop();
-                    value += valueStack.pop();
-                }
-                stack.pop();
-                stack.push('v');
-                value = value == 0 ? 1 : value * 2;
-                valueStack.push(value);
+                stack.push('(');
+                continue;
             }
+
+            int value = 0;
+            while (!stack.isEmpty() && stack.peek() == vC) {
+                stack.pop();
+                value += valueStack.pop();
+            }
+            stack.pop();
+            stack.push(vC);
+            valueStack.push(value == 0 ? 1 : 2 * value);
         }
+
         int result = 0;
         while (!valueStack.isEmpty()) {
             result += valueStack.pop();
