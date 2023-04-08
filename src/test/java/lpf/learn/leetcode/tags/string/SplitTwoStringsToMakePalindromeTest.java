4c2c754028aa5af6ddc5365ca35678c7ea7d9025
liupf <506629790@qq.com>
2023-04-08 12:56:01 +0800

    1、leetcode

---
+++ src/test/java/lpf/learn/leetcode/tags/string/SplitTwoStringsToMakePalindromeTest.java
@@ -0,0 +1,46 @@
+package lpf.learn.leetcode.tags.string;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author liupf
+ * @date 2021-04-10 20:04
+ */
+public class SplitTwoStringsToMakePalindromeTest {
+
+    @Test
+    public void test1() {
+        SplitTwoStringsToMakePalindrome temp = new SplitTwoStringsToMakePalindrome();
+        Assert.assertTrue(temp.checkPalindromeFormation("x", "y"));
+    }
+
+    @Test
+    public void test2() {
+        SplitTwoStringsToMakePalindrome temp = new SplitTwoStringsToMakePalindrome();
+        Assert.assertFalse(temp.checkPalindromeFormation("xbdef", "xecab"));
+    }
+
+    @Test
+    public void test3() {
+        SplitTwoStringsToMakePalindrome temp = new SplitTwoStringsToMakePalindrome();
+        Assert.assertTrue(temp.checkPalindromeFormation("ulacfd", "jizalu"));
+    }
+
+    @Test
+    public void test4() {
+        SplitTwoStringsToMakePalindrome temp = new SplitTwoStringsToMakePalindrome();
+        Assert.assertTrue(temp.checkPalindromeFormation("abdef", "fecab"));
+    }
+
+    @Test
+    public void test5() {
+        SplitTwoStringsToMakePalindrome temp = new SplitTwoStringsToMakePalindrome();
+        Assert.assertTrue(temp.checkPalindromeFormation("pvhmupgqeltozftlmfjjde", "yjgpzbezspnnpszebzmhvp"));
+    }
+    @Test
+    public void test6() {
+        SplitTwoStringsToMakePalindrome temp = new SplitTwoStringsToMakePalindrome();
+        Assert.assertFalse(temp.checkPalindromeFormation("abda", "acmc"));
+    }
+}
