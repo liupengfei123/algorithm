910d15ed1401e8b97edbe6f982e099fa85fc7a99
刘鹏飞 <liupf@bonree.com>
2023-04-23 14:27:12 +0800

    leetcode 练习

--- src/test/java/lpf/learn/leetcode/tags/dp/FillingBookcaseShelvesTest.java
+++ src/test/java/lpf/learn/leetcode/tags/dp/FillingBookcaseShelvesTest.java
@@ -9,4 +9,10 @@ public class FillingBookcaseShelvesTest {
         FillingBookcaseShelves test = new FillingBookcaseShelves();
         Assert.assertEquals(6, test.minHeightShelves(new int[][]{{1,1},{2,3},{2,3},{1,1},{1,1},{1,1},{1,2}}, 4));
     }
+
+    @Test
+    public void test2(){
+        FillingBookcaseShelves test = new FillingBookcaseShelves();
+        Assert.assertEquals(4, test.minHeightShelves(new int[][]{{1,3},{2,4},{3,2}}, 6));
+    }
 }
