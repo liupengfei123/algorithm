d5bf9608d8d2e67c1e604229615dfa9f8e9a780a
刘鹏飞 <liupf@bonree.com>
2023-07-13 11:19:04 +0800

    leetcode 练习

--- src/test/java/lpf/learn/leetcode/tags/dp/MinimumFallingPathSumTest.java
+++ src/test/java/lpf/learn/leetcode/tags/dp/MinimumFallingPathSumTest.java
@@ -1,6 +1,5 @@
 package lpf.learn.leetcode.tags.dp;
 
-import lpf.learn.leetcode.tags.array.MinimumFallingPathSum;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -22,25 +21,4 @@ public class MinimumFallingPathSumTest {
         MinimumFallingPathSum test = new MinimumFallingPathSum();
         Assert.assertEquals(-48, test.minFallingPathSum(new int[][]{{-48}}));
     }
-
-    @Test
-    public void test4(){
-        ArithmeticSlices test = new ArithmeticSlices();
-        Assert.assertEquals(11, test.numberOfArithmeticSlices(new int[]{1, 2, 3, 4, 5, 6, 8, 10}));
-    }
-
-    @Test
-    public void test5(){
-        ArithmeticSlices test = new ArithmeticSlices();
-        Assert.assertEquals(3, test.numberOfArithmeticSlices(new int[]{7, 7, 7, 7}));
-    }
-
-    @Test
-    public void test6(){
-        ArithmeticSlices test = new ArithmeticSlices();
-        Assert.assertEquals(0, test.numberOfArithmeticSlices(new int[]{1, 1, 2, 5, 7}));
-    }
-
-
-
 }
