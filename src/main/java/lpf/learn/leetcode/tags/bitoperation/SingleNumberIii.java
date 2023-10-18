3c0194a3f8f18a28c050a4d20262ff4b132f1f55
刘鹏飞 <liupf@bonree.com>
2023-10-18 10:15:01 +0800

    leetcode 练习

---
+++ src/main/java/lpf/learn/leetcode/tags/bitoperation/SingleNumberIii.java
@@ -0,0 +1,53 @@
+package lpf.learn.leetcode.tags.bitoperation;
+
+/** 260 只出现一次的数字 III
+ <p>给你一个整数数组&nbsp;<code>nums</code>，其中恰好有两个元素只出现一次，其余所有元素均出现两次。 找出只出现一次的那两个元素。你可以按 <strong>任意顺序</strong> 返回答案。</p>
+
+ <p>你必须设计并实现线性时间复杂度的算法且仅使用常量额外空间来解决此问题。</p>
+
+ <p><strong>示例 1：</strong></p>
+ <pre>
+ <strong>输入：</strong>nums = [1,2,1,3,2,5]
+ <strong>输出：</strong>[3,5]
+ <strong>解释：</strong>[5, 3] 也是有效的答案。
+ </pre>
+
+ <p><strong>示例 2：</strong></p>
+ <pre>
+ <strong>输入：</strong>nums = [-1,0]
+ <strong>输出：</strong>[-1,0]
+ </pre>
+
+ <p><strong>示例 3：</strong></p>
+ <pre>
+ <strong>输入：</strong>nums = [0,1]
+ <strong>输出：</strong>[1,0]
+ </pre>
+
+ <p><strong>提示：</strong></p>
+ <ul>
+ <li><code>2 &lt;= nums.length &lt;= 3 * 10<sup>4</sup></code></li>
+ <li><code>-2<sup>31</sup> &lt;= nums[i] &lt;= 2<sup>31</sup> - 1</code></li>
+ <li>除两个只出现一次的整数外，<code>nums</code> 中的其他数字都出现两次</li>
+ </ul>
+ */
+public class SingleNumberIii {
+
+    public int[] singleNumber(int[] nums) {
+        int xorsum = 0;
+        for (int num : nums) xorsum ^= num;
+
+        // 防止溢出
+        int lsb = xorsum & (-xorsum);
+        int res1 = 0, res2 = 0;
+        for (int num : nums) {
+            if ((num & lsb) > 0) {
+                res1 ^= num;
+            } else {
+                res2 ^= num;
+            }
+        }
+        return new int[]{res1, res2};
+    }
+
+}
