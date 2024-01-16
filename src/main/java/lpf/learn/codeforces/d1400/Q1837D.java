6ed8af3ce3a22f2deee0b5a8ea785bffb51842b5
刘鹏飞 <liupf@bonree.com>
2024-01-16 10:18:42 +0800

    codeforces 练习

--- src/main/java/lpf/learn/codeforces/d1400/Q1837D.java
+++ src/main/java/lpf/learn/codeforces/d1400/Q1837D.java
@@ -3,6 +3,9 @@ package lpf.learn.codeforces.d1400;
 import java.io.*;
 import java.util.*;
 
+/**
+ * https://codeforces.com/problemset/problem/1837/D
+ */
 public class Q1837D {
     private final static Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(System.in)));
     private final static PrintWriter out = new PrintWriter(new BufferedOutputStream(System.out));
@@ -11,17 +14,47 @@ public class Q1837D {
         int t = sc.nextInt();
 
         while (t-- > 0) {
-            int n = sc.nextInt();
-            String s = sc.next();
-
-            solve(n, s);
+            solve(sc.nextInt(), sc.next().toCharArray());
         }
         out.close();
     }
 
+    private static void solve(int n, char[] chars) {
+        int cnt = 0;
+        for (char aChar : chars) cnt += aChar == '(' ? 1 : -1;
+        if (cnt != 0) {
+            out.println(-1);
+            return;
+        }
 
-    private static void solve(int n, String m) {
-        char[] chars = m.toCharArray();
+        int[] value = new int[n];
+        Arrays.fill(value, 2);
+
+        for (int i = 0; i < n; i++) {
+            if (chars[i] == '(') {
+                if (cnt >= 0) value[i] = 1;
+                cnt++;
+            } else {
+                if (cnt > 0) value[i] = 1;
+                cnt--;
+            }
+        }
+
+        int temp = 0;
+        for (int i = 0; i < n; i++) temp |= value[i];
+
+        if (temp == 3) {
+            out.println(2);
+            for (int i = 0; i < n; i++) out.print(value[i] + " ");
+        } else {
+            out.println(1);
+            for (int i = 0; i < n; i++) out.print("1 ");
+        }
+        out.println();
+    }
+
+
+    private static void solve2(int n, char[] chars) {
         int[] ans = new int[n];
         int cnt = 0, k = 0;
         for (int i = 0, lst = cnt; i < n; i++, lst = cnt) {
@@ -42,45 +75,4 @@ public class Q1837D {
         out.println();
     }
 
-    private static void solve2(int n, String m) {
-        char[] chars = m.toCharArray();
-        int c = 0;
-        for (int i = 0; i < n; i++) if (chars[i] == ')') c++;
-        if (c << 1 != n) {
-            out.println(-1);
-            return;
-        }
-        int[] ans = new int[n];
-
-        int lc = 0, rc = 0;
-        int flag = 0;
-        for (int i = 0; i < n; i++) {
-            if (chars[i] == ')') {
-                if (lc > 0) {
-                    ans[i] = 1;
-                    lc--;
-                } else {
-                    ans[i] = 2;
-                    rc++;
-                }
-            } else {
-                if (rc > 0) {
-                    ans[i] = 2;
-                    rc--;
-                } else {
-                    ans[i] = 1;
-                    lc++;
-                }
-            }
-            flag |= ans[i];
-        }
-        if (flag == 3) {
-            out.println(2);
-        } else {
-            out.println(1);
-            Arrays.fill(ans, 1);
-        }
-        for (int i = 0; i < n; i++) out.print(ans[i] + " ");
-        out.println();
-    }
 }
