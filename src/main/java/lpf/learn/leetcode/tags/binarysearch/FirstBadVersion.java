bfd8896a89d4b157d017fade2ca7f7c2cb9dfd03
liupf <506629790@qq.com>
2024-12-28 19:41:16 +0800

    leetcode 练习

--- src/main/java/lpf/learn/leetcode/tags/binarysearch/FirstBadVersion.java
+++ src/main/java/lpf/learn/leetcode/tags/binarysearch/FirstBadVersion.java
@@ -1,57 +1,55 @@
 package lpf.learn.leetcode.tags.binarysearch;
 
-/** [278] 第一个错误的版本
- * 你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，
- * 所以错误的版本之后的所有版本都是错的。
- *
- * 假设你有 n 个版本 [1, 2, ..., n]，你想找出导致之后所有版本出错的第一个错误的版本。
- * 你可以通过调用 bool isBadVersion(version) 接口来判断版本号 version 是否在单元测试中出错。实现一个函数来查找第一个错误
- * 的版本。你应该尽量减少对调用 API 的次数。
- *
- * 示例 1：
- * 输入：n = 5, bad = 4
- * 输出：4
- * 解释：
- * 调用 isBadVersion(3) -> false
- * 调用 isBadVersion(5) -> true
- * 调用 isBadVersion(4) -> true
- * 所以，4 是第一个错误的版本。
- *
- * 示例 2：
- * 输入：n = 1, bad = 1
- * 输出：1
- *
- *
- * 提示：
- * 1 <= bad <= n <= 2³¹ - 1
+
+/** [278]第一个错误的版本
+ <p>你是产品经理，目前正在带领一个团队开发新的产品。不幸的是，你的产品的最新版本没有通过质量检测。由于每个版本都是基于之前的版本开发的，所以错误的版本之后的所有版本都是错的。</p>
+ <p>假设你有 <code>n</code> 个版本 <code>[1, 2, ..., n]</code>，你想找出导致之后所有版本出错的第一个错误的版本。</p>
+ <p>你可以通过调用&nbsp;<code>bool isBadVersion(version)</code>&nbsp;接口来判断版本号 <code>version</code> 是否在单元测试中出错。实现一个函数来查找第一个错误的版本。你应该尽量减少对调用 API 的次数。</p> &nbsp;
+
+ <p><strong class="example">示例 1：</strong></p>
+ <pre>
+ <strong>输入：</strong>n = 5, bad = 4
+ <strong>输出：</strong>4
+ <strong>解释：</strong>
+ <span><code>调用 isBadVersion(3) -&gt; false
+ 调用 isBadVersion(5)&nbsp;-&gt; true
+ 调用 isBadVersion(4)&nbsp;-&gt; true</code></span>
+ <span><code>所以，4 是第一个错误的版本。</code></span>
+ </pre>
+
+ <p><strong class="example">示例 2：</strong></p>
+ <pre>
+ <strong>输入：</strong>n = 1, bad = 1
+ <strong>输出：</strong>1
+ </pre>
+
+ <p><strong>提示：</strong></p>
+ <ul>
+ <li><code>1 &lt;= bad &lt;= n &lt;= 2<sup>31</sup> - 1</code></li>
+ </ul>
  */
 public class FirstBadVersion {
-
+    private int badVersion;
+    public FirstBadVersion(int x) {
+        badVersion = x;
+    }
 
     public int firstBadVersion(int n) {
-        int left = 1, right = n;
-
-        while (left < right) {
-            int mid = (right - left) / 2 + left;
-
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
             if (isBadVersion(mid)) {
-                right = mid;
+                r = mid;
             } else {
-                left = mid + 1;
+                l = mid + 1;
             }
         }
-        return right;
+        return r;
+    }
+
+    private boolean isBadVersion(int x) {
+        return x >= badVersion;
     }
 
 
-
-    private int badVersion;
-
-    public FirstBadVersion(int badVersion) {
-        this.badVersion = badVersion;
-    }
-
-     private boolean isBadVersion(int version) {
-        return version >= badVersion;
-    }
 }
--- src/main/java/lpf/learn/leetcode/tags/binarysearch/GuessNumberHigherOrLower.java
+++ src/main/java/lpf/learn/leetcode/tags/binarysearch/GuessNumberHigherOrLower.java
@@ -1,56 +1,54 @@
 package lpf.learn.leetcode.tags.binarysearch;
 
+
 /** [374]猜数字大小
- * 猜数字游戏的规则如下：
- * 每轮游戏，我都会从 1 到 n 随机选择一个数字。 请你猜选出的是哪个数字。
- * 如果你猜错了，我会告诉你，你猜测的数字比我选出的数字是大了还是小了。
- *
- * 你可以通过调用一个预先定义好的接口 int guess(int num) 来获取猜测结果，返回值一共有 3 种可能的情况（-1，1 或 0）：
- * -1：我选出的数字比你猜的数字小 pick < num
- * 1：我选出的数字比你猜的数字大 pick > num
- * 0：我选出的数字和你猜的数字一样。恭喜！你猜对了！pick == num
- *
- * 返回我选出的数字。
- *
- * 示例 1：
- * 输入：n = 10, pick = 6
- * 输出：6
- *
- * 示例 2：
- * 输入：n = 1, pick = 1
- * 输出：1
- *
- * 示例 3：
- * 输入：n = 2, pick = 1
- * 输出：1
- *
- * 示例 4：
- * 输入：n = 2, pick = 2
- * 输出：2
- *
- * 提示：
- * 1 <= n <= 2^31 - 1
- * 1 <= pick <= n
+ <p>我们正在玩猜数字游戏。猜数字游戏的规则如下：</p>
+ <p>我会从&nbsp;<code>1</code>&nbsp;到&nbsp;<code>n</code> 随机选择一个数字。 请你猜选出的是哪个数字。</p>
+ <p>如果你猜错了，我会告诉你，我选出的数字比你猜测的数字大了还是小了。</p>
+ <p>你可以通过调用一个预先定义好的接口 <code>int guess(int num)</code> 来获取猜测结果，返回值一共有三种可能的情况：</p>
+ <ul>
+ <li><code>-1</code>：你猜的数字比我选出的数字大 （即&nbsp;<code>num &gt; pick</code>）。</li>
+ <li><code>1</code>：你猜的数字比我选出的数字小&nbsp;（即&nbsp;<code>num &lt;&nbsp;pick</code>）。</li>
+ <li><code>0</code>：你猜的数字与我选出的数字相等。（即&nbsp;<code>num&nbsp;== pick</code>）。</li>
+ </ul>
+
+ <p>返回我选出的数字。</p>
+
+ <p><strong>示例 1：</strong></p>
+ <pre>
+ <strong>输入：</strong>n = 10, pick = 6
+ <strong>输出：</strong>6
+ </pre>
+
+ <p><strong>示例 2：</strong></p>
+ <pre>
+ <strong>输入：</strong>n = 1, pick = 1
+ <strong>输出：</strong>1
+ </pre>
+
+ <p><strong>示例 3：</strong></p>
+ <pre>
+ <strong>输入：</strong>n = 2, pick = 1
+ <strong>输出：</strong>1
+ </pre>
+
+ <p><strong>提示：</strong></p>
+ <ul>
+ <li><code>1 &lt;= n &lt;= 2<sup>31</sup> - 1</code></li>
+ <li><code>1 &lt;= pick &lt;= n</code></li>
+ </ul>
  */
 public class GuessNumberHigherOrLower {
 
     public int guessNumber(int n) {
-        int left = 1;
-        int right = n;
+        int l = 1, r = n;
+        while (l < r) {
+            int mid = l + (r - l) / 2;
 
-        while (left < right) {
-            int mid = (right - left) / 2 + left;
-
-            int guess = guess(mid);
-            if (guess > 0) {
-                left = mid + 1;
-            } else if (guess < 0) {
-                right = mid;
-            } else {
-                return mid;
-            }
+            if (guess(mid) <= 0) r = mid;
+            else l = mid + 1;
         }
-        return left;
+        return r;
     }
 
 
